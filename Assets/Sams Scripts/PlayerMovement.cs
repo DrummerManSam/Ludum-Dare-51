04b6ad73730a47efa7c938d7871b2a063fc38f3b
Sam Stephenson <samstephenson77@gmail.com>
2022-09-30 19:58:28 -0400

    Sam_2

--- Assets/Sams Scripts/PlayerMovement.cs
+++ Assets/Sams Scripts/PlayerMovement.cs
@@ -5,33 +5,33 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private int speed = 5;
+    [SerializeField]
+    public int speed = 5;
     private Vector2 movement;
     private Rigidbody rb;
-    private Animator animator;
+    
+    
     
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
-        animator = GetComponent<Animator>();
+        
     }
 
-    private void OnMovement(InputValue input)
+    public void OnMovement(InputValue input)
     {
-        movement = input.Get<Vector3>();
-
-        /*animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);*/
+        movement = input.Get<Vector2>();
         
+          
     }
 
     private void FixedUpdate()
     {
-        Vector3 moveInput = new Vector3(movement.x, 0, 0);
-        rb.MovePosition(transform.position + moveInput  * speed * Time.fixedDeltaTime);
+        //Vector3 moveInput = new Vector3(movement.x, 0, 0);
+        rb.MovePosition(rb.position + new Vector3(movement.x, movement.y, 0) * speed * Time.fixedDeltaTime);
     }
 
 }
+
 
