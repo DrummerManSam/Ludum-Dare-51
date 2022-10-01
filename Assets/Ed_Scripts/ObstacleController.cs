ae88951e1ea650a4b59f07784b4ccfba8d0cb75d
EdVP <djedwardp@aol.com>
2022-10-01 12:02:48 -0400

    Ed_22

--- Assets/Ed_Scripts/ObstacleController.cs
+++ Assets/Ed_Scripts/ObstacleController.cs
@@ -7,9 +7,21 @@ public class ObstacleController : MonoBehaviour
     [SerializeField]
     private float m_obstacleSpeed = 1f;
 
+    private Rigidbody rb;
+
+    public void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void OnEnable()
+    {
+       // rb.AddForce((m_obstacleSpeed * GameManager.instance.globalSpeed) * -transform.forward * Time.deltaTime, ForceMode.Impulse);
+    }
+
     public void Update()
     {
-        transform.position += (m_obstacleSpeed * GameManager.instance.globalSpeed) * -transform.forward * Time.deltaTime;
+        rb.AddForce((m_obstacleSpeed * GameManager.instance.globalSpeed) * -transform.forward * Time.deltaTime, ForceMode.Impulse);
     }
 
     public void OnHit()
