64a1a461045209c91501350e2b0a287b21e9ddfc
EdVP <djedwardp@aol.com>
2022-09-30 20:16:35 -0400

    Ed_5

--- Assets/Sams Scripts/PlayerMovement.cs
+++ Assets/Sams Scripts/PlayerMovement.cs
@@ -7,12 +7,15 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
     public int speed = 5;
+
+    [SerializeField]
     private Vector2 movement;
     private Rigidbody rb;
-    
-    
-    
 
+    [SerializeField]
+    private Vector3 finalMovement;
+    
+    
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
@@ -22,14 +25,13 @@ public class PlayerMovement : MonoBehaviour
     public void OnMovement(InputValue input)
     {
         movement = input.Get<Vector2>();
-        
-          
+
+        finalMovement = new Vector3(movement.x, movement.y, 0f) * Time.deltaTime * speed;
     }
 
     private void FixedUpdate()
     {
-        //Vector3 moveInput = new Vector3(movement.x, 0, 0);
-        rb.MovePosition(rb.position + new Vector3(movement.x, movement.y, 0) * speed * Time.fixedDeltaTime);
+        rb.AddForce(finalMovement, ForceMode.Impulse);
     }
 
 }
